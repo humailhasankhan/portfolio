ea4bb1a4de7b60d0b146d0ca1e8969cec155f7e7
humailhasankhan <h.hasan.khan88@hotmail.com>
2022-03-13 18:43:53 +0000

    add animation to loading page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import Loader from "./components/Loader";
+import Main from "./components/Main";
 
 function App() {
   return (
     <div className="App">
       <Loader />
+      <Main />
     </div>
   );
 }
