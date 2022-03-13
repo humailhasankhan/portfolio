ab3124b71f807adcba2abd746f8cc29339e57366
humailhasankhan <h.hasan.khan88@hotmail.com>
2022-03-13 22:03:14 +0000

    add framer motion

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,21 +1,28 @@
 import React, { useState, useEffect } from "react";
+import AnimateComponent from "./AnimateComponent";
+import { motion, AnimatePresence } from "framer-motion";
 
 const Main = () => {
   const [showComponent, setShowComponent] = useState(false);
-
-  useEffect(() => {
-    setInterval(() => {
-      setShowComponent(!showComponent);
-    }, 8000);
-  }, []);
+  setTimeout(() => {
+    setShowComponent(true);
+  }, 8000);
+  useEffect(() => {}, [showComponent]);
 
   return (
     <>
-      {showComponent && (
-        <div>
-          <h1>Hello how are you</h1>
-        </div>
-      )}
+      <AnimateComponent>
+        <AnimatePresence>
+          {showComponent && (
+            <motion.div
+              initial={{ opacity: 1, x: 0 }}
+              transition={{ duration: 1 }}
+            >
+              <h1>Hello how are you</h1>
+            </motion.div>
+          )}
+        </AnimatePresence>
+      </AnimateComponent>
     </>
   );
 };
