7a325f2643bee736f9ec9f998f513a480be3db28
humailhasankhan <h.hasan.khan88@hotmail.com>
2022-03-19 17:48:43 +0000

    edit contact component

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,10 +1,55 @@
 import React from "react";
+import "./Contact.css";
 
-const Contact = () => {
+const Contact = (props) => {
   return (
-    <div class="container">
-      <div>
-        <h1>LET'S TALK</h1>
+    <div className="form-section">
+      <div className="form-container">
+        <form action="" onSubmit="">
+          <div className="form-area">
+            <h1>LET'S TALK!</h1>
+            <br />
+            <input
+              className="input100"
+              type="text"
+              name="fullName"
+              placeholder="Full Name"
+              required
+            />
+            <br />
+            <br />
+            <input
+              className="input100"
+              type="text"
+              name="phone"
+              placeholder="Phone Number"
+              required
+            />
+            <br />
+            <br />
+            <input
+              className="input100"
+              type="text"
+              name="email"
+              placeholder="Enter Email"
+              required
+            />
+            <br />
+          </div>
+          <div className="formWord">
+            <br />
+            <textarea
+              className="message"
+              name="message"
+              placeholder="Message"
+              required
+            ></textarea>
+            <br />
+            <button className="submit-button">SUBMIT</button>
+
+            <div className="row"></div>
+          </div>
+        </form>
       </div>
     </div>
   );
