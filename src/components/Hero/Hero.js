a0d5541488fcd7077845a7113a0f1f745a94c5c1
humailhasankhan <h.hasan.khan88@hotmail.com>
2022-03-19 15:34:52 +0000

    adjust hero content

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -10,34 +10,36 @@ import {
 const Hero = () => {
   return (
     <div className="hero">
-      <div className="hero-inner">
-        <h1>
-          <span id="span-one">
-            I'm <span id="span-two">Humail Khan</span>
-          </span>
-        </h1>
-        <span className="description">Full stack web developer</span>
-        <div>
-          <button className="hero-button">Resumé</button>
-        </div>
-        <div className="hero-icons">
-          <div className="icon-spacing">
-            <FontAwesomeIcon
-              className="media-icons github-icon"
-              icon={faGithub}
-            ></FontAwesomeIcon>
+      <div className="inner-container">
+        <div className="hero-inner">
+          <h1>
+            <span id="span-one">
+              I'm <span id="span-two">Humail Khan</span>
+            </span>
+          </h1>
+          <span className="description">Full stack web developer</span>
+          <div>
+            <button className="hero-button">Resumé</button>
           </div>
-          <div className="icon-spacing">
-            <FontAwesomeIcon
-              className="media-icons linkedin-icon"
-              icon={faLinkedin}
-            ></FontAwesomeIcon>
-          </div>
-          <div className="icon-spacing twitter-icon">
-            <FontAwesomeIcon
-              className="media-icons"
-              icon={faTwitter}
-            ></FontAwesomeIcon>
+          <div className="hero-icons">
+            <div className="icon-spacing">
+              <FontAwesomeIcon
+                className="media-icons github-icon"
+                icon={faGithub}
+              ></FontAwesomeIcon>
+            </div>
+            <div className="icon-spacing">
+              <FontAwesomeIcon
+                className="media-icons linkedin-icon"
+                icon={faLinkedin}
+              ></FontAwesomeIcon>
+            </div>
+            <div className="icon-spacing twitter-icon">
+              <FontAwesomeIcon
+                className="media-icons"
+                icon={faTwitter}
+              ></FontAwesomeIcon>
+            </div>
           </div>
         </div>
       </div>
